f9f79c914a6d4c81ef6a33b0e3bba0ee62e921d5
Minh/Mariah Nguyen <ncpminh3@gmail.com>
2023-01-29 14:45:43 -0800

    this is a message

---
+++ HelloWorld03.py
@@ -0,0 +1,11 @@
+# dH 1/28/23
+# HelloWorld03.py
+#Minh Nguyen 0920131
+# Sample code:
+user_name = input("Hello, please enter your first and last name: ")
+print("\n Hello, " + user_name)
+user_school = input("\n which school are you at? ")
+user_age = input("\n How old are you, " + user_name + "? ")
+print("Nice to know you, " + user_name)
+
+
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This is the demo for SP23 CIT-95 class
 
 This is an add to README.md file
+
+This file was changed.
